77d7c30f48379a89dcbd30f0e18d804669dd439e
Teneppa <laurilateemu8@gmail.com>
2019-01-03 13:04:57 +0200

    Added oled library

--- openTileEngine.ino
+++ openTileEngine.ino
@@ -3,12 +3,24 @@
 /*
     Name:       openTileEngine.ino
     Created:	3.1.2019 12.03.58
-    Author:     TEUKKA-PC\lauri
+    Author:     Teemu Laurila
 */
 
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include "variableType.h"
 #include "tilengine.h"
 #include "gameObject.h"
+
+/*<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//==- Oled things -==\\<<//>>\\<<//>>\\<<//>>\\<<//>>*/
+
+const uint8_t OLED_DC = 6;
+const uint8_t OLED_CS = 7;
+const uint8_t OLED_RESET = 8;
+
+Adafruit_SSD1306 oled(OLED_DC, OLED_RESET, OLED_CS);
+
+/*<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>\\<<//>>*/
 
 tilengine engine(16, 8, 8, 8);	// tilengine(kartan leveys, kartan korkeus,
 								//			 laata leveys, laatan korkeus);
